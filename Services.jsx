66f0a45b3b10b6a51294d2caf97f8b97196ab8ff
Whitex4 <157428459+HumaSiddique@users.noreply.github.com>
2024-07-20 12:56:39 -0700

    Update Services.jsx

--- Services.jsx
+++ Services.jsx
@@ -1,32 +1,28 @@
-
-
 const Services = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-10 p-8">
-     <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-2 hover:bg-white">
-      <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12" >
-  <h2 className="text-3xl font-bold mb-4">Frontend Development</h2>
-  <p className="text-black">Our frontend development services excel in crafting visually stunning, responsive, and user-centric interfaces. Utilizing cutting-edge technologies like HTML5, CSS3, JavaScript, and frameworks such as React, Angular, and Vue.js, we build dynamic and interactive websites. Our expert developers focus on performance, accessibility, and cross-browser compatibility, ensuring clean, scalable, and maintainable code. Whether a simple landing page or a complex web application, we deliver high-quality solutions tailored to meet your business needs.</p>
-</div>
-
-
-      </section>
-      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-2 hover:bg-white">
-        <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
-          <h2 className="text-3xl font-bold mb-4">Backend Development</h2>
-          <p className="text-black">Our backend services ensure robust and scalable server-side solutions using technologies like Node.js, Express, and MongoDB. We specialize in API development, server architecture, and cloud integration, providing secure and efficient data management. Trust us to power your applications with reliable performance and advanced functionalities.</p>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6 p-4">
+      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-4 hover:bg-white">
+        <div className="container mx-auto p-2 md:p-4 lg:p-6">
+          <h2 className="text-2xl font-bold mb-2">Frontend Development</h2>
+          <p className="text-black text-sm">Our frontend development services excel in crafting visually stunning, responsive, and user-centric interfaces. Utilizing cutting-edge technologies like HTML5, CSS3, JavaScript, and frameworks such as React, Angular, and Vue.js, we build dynamic and interactive websites. Our expert developers focus on performance, accessibility, and cross-browser compatibility, ensuring clean, scalable, and maintainable code. Whether a simple landing page or a complex web application, we deliver high-quality solutions tailored to meet your business needs.</p>
         </div>
       </section>
-      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-2 hover:bg-white">
-        <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
-          <h2 className="text-3xl font-bold mb-4">JavaScript</h2>
-          <p className="text-black">Our Java development services deliver high-performance, secure, and scalable applications tailored to your needs. Leveraging the robust capabilities of Java, we build enterprise solutions, web applications, and backend systems with seamless integration and optimal efficiency. Trust us to provide innovative and reliable Java solutions for your business.</p>
+      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-4 hover:bg-white">
+        <div className="container mx-auto p-2 md:p-4 lg:p-6">
+          <h2 className="text-2xl font-bold mb-2">Backend Development</h2>
+          <p className="text-black text-sm">Our backend services ensure robust and scalable server-side solutions using technologies like Node.js, Express, and MongoDB. We specialize in API development, server architecture, and cloud integration, providing secure and efficient data management. Trust us to power your applications with reliable performance and advanced functionalities.</p>
         </div>
       </section>
-      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-2 hover:bg-white">
-        <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
-          <h2 className="text-3xl font-bold mb-4">React</h2>
-          <p className="text-black">Our React services are designed to elevate your digital presence with cutting-edge solutions. From crafting responsive UI components to optimizing performance, we are dedicated to delivering impactful React applications. Let us empower your business with robust, scalable, and user-friendly React solutions that drive growth and exceed expectations.</p>
+      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-4 hover:bg-white">
+        <div className="container mx-auto p-2 md:p-4 lg:p-6">
+          <h2 className="text-2xl font-bold mb-2">JavaScript</h2>
+          <p className="text-black text-sm">Our JavaScript services deliver high-performance, secure, and scalable applications tailored to your needs. Leveraging the robust capabilities of JavaScript, we build enterprise solutions, web applications, and backend systems with seamless integration and optimal efficiency. Trust us to provide innovative and reliable JavaScript solutions for your business.</p>
+        </div>
+      </section>
+      <section className="bg-teal-600 rounded-xl shadow-md overflow-hidden hover:animate-pulse p-4 hover:bg-white">
+        <div className="container mx-auto p-2 md:p-4 lg:p-6">
+          <h2 className="text-2xl font-bold mb-2">React</h2>
+          <p className="text-black text-sm">Our React services are designed to elevate your digital presence with cutting-edge solutions. From crafting responsive UI components to optimizing performance, we are dedicated to delivering impactful React applications. Let us empower your business with robust, scalable, and user-friendly React solutions that drive growth and exceed expectations.</p>
         </div>
       </section>
     </div>
@@ -34,5 +30,3 @@ const Services = () => {
 }
 
 export default Services;
-
-
